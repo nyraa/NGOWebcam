3f4e2f2d34501437f459fafcb2a435bf28e99b1e
nyraa <112930946+nyraa@users.noreply.github.com>
2024-08-12 16:44:06 +0800

    update window.h

--- src/window.h
+++ src/window.h
@@ -3,13 +3,8 @@
 #include <X11/Xlib.h>
 #include <cairo/cairo.h>
 
-int updateFrame();
 void drawImage(Window window, cairo_surface_t *image, int width, int height, cairo_surface_t *screenSaver, cairo_surface_t *ame);
-void changeAnimation(animation_t target);
-void unloadFrames();
-void loadFrames();
 void loadBackground();
 int handleWindow(Display *display, Window window);
 int init(Display **display, Window *window);
 void destroy();
-unsigned long long getMilliseconds();
