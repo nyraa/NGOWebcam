7d423a99a89fbeddd767c4ae27f7a7f468db60b1
nyraa <112930946+nyraa@users.noreply.github.com>
2024-08-12 15:41:28 +0800

    update include chain

--- src/window.h
+++ src/window.h
@@ -1,9 +1,11 @@
+#include "stream.h"
+
 #include <X11/Xlib.h>
 #include <cairo/cairo.h>
 
 int updateFrame();
 void drawImage(Window window, cairo_surface_t *image, int width, int height, cairo_surface_t *screenSaver, cairo_surface_t *ame);
-void changeAnimation(int target);
+void changeAnimation(animation_t target);
 void unloadFrames();
 void loadFrames();
 void loadBackground();
